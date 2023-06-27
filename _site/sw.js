8a375b80d3412946f887514a2434ecef65b4a3cf
yeongjunekoh <rhdudwns0@naver.com>
2023-06-27 17:27:42 +0900

    [Change] change _config.yml

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230627.161220';
+const cacheName = 'chirpy-20230627.162458';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
